cd3c87d903d4ab818a4a6e5902be438362b60d35
almac2022 <al@newgraphenvironment.com>
2025-11-11 15:11:36 -0800

    switch to git token

--- R/ngr_git_issue_details.R
+++ R/ngr_git_issue_details.R
@@ -3,7 +3,7 @@
 #' This function retrieves the timeline or event details for a specified GitHub issue using the GitHub API. By default, it filters for events associated with commits only.
 #'
 #' @param issue_url_json [character]. The full GitHub API URL of the issue (e.g., "https://api.github.com/repos/user/repo/issues/123").
-#' @param token [character]. A GitHub personal access token (PAT). Default is `Sys.getenv("GITHUB_PAT")`.
+#' @param token [character]. A GitHub personal access token (PAT). Default is `gh::gh_token()`.
 #' @param endpoint [character]. The API endpoint to use for retrieving issue events. Defaults to "timeline".
 #' to access administrative actions like mentioned, labeling or assigning.
 #' @param events_all [logical]. If `TRUE`, return all events. If `FALSE` (default), return only those associated with commits (i.e., with a non-null `commit_id`).
@@ -13,6 +13,7 @@
 #' @importFrom gh gh
 #' @importFrom purrr map_dfr
 #' @importFrom tibble tibble
+#'@importFrom gh gh_token
 #' @importFrom chk chk_string chk_flag chk_not_null
 #' @export
 #'
@@ -24,7 +25,7 @@
 #' @examples
 #' ngr_git_issue_details("https://api.github.com/repos/NewGraphEnvironment/fpr/issues/13")
 #' ngr_git_issue_details("https://api.github.com/repos/NewGraphEnvironment/fpr/issues/13", events_all = TRUE)
-ngr_git_issue_details <- function(issue_url_json, token = Sys.getenv("GITHUB_PAT"), endpoint = "timeline", events_all = FALSE) {
+ngr_git_issue_details <- function(issue_url_json, token = gh::gh_token(), endpoint = "timeline", events_all = FALSE) {
   chk::chk_not_null(issue_url_json)
   chk::chk_string(issue_url_json)
   if (!is.null(token)) chk::chk_string(token)
